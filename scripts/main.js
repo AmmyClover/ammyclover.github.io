0d70d2aa8c197c70f6dd3cee25db296629c92b89
AmmyClover <ammy_clover@mail.ru>
2022-12-09 23:48:33 +0400

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,4 @@
 
-
 var myHtml = document.querySelector('html');
 var myImage = document.querySelector('img');
 
@@ -13,22 +12,65 @@ myHtml.onwheel= function(){
     }
 }
 
-var myButton = document.querySelector('button');
+
+
+var keyBox = document.getElementById("username");
+
+function onchange(e){
+    // получаем новое значение
+    // var val = e.target.value;
+    // установка значения
+    console.log(keyBox.textContent)
+}
+
+function onblur(e){
+     
+    // получаем его значение и обрезаем все пробелы
+    var text = keyBox.value.trim();
+    if(text==="")
+        keyBox.style.borderColor = "red";
+    else
+        keyBox.style.borderColor = "green";
+    
+}
+
+function onfocus(e){
+    // установка цвета границ поля
+    keyBox.style.borderColor = "blue";
+}
+
+var val
 var myHeading = document.querySelector('h1');
 
-function setUserName() {
-    var myName = prompt('Please enter your name.');
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'Zdarova, ' + myName;
+function setUserName(val) {
+    localStorage.setItem('name', val);
+    myHeading.textContent = 'Zdarova, ' + val;
 }
 
 if(!localStorage.getItem('name')) {
-    setUserName();
+    
 } else {
     var storedName = localStorage.getItem('name');
     myHeading.textContent = 'Zdarova, ' + storedName;
 }
 
-myButton.onclick = function() {
-    setUserName();
+function onkeydown(e)
+{
+    if (e.code === 'Enter') {
+        val = keyBox.value;
+        setUserName(val);
+    }
 }
+
+
+keyBox.addEventListener("change", onchange);
+keyBox.addEventListener("blur", onblur);
+keyBox.addEventListener("focus", onfocus);
+keyBox.addEventListener("keydown", onkeydown); 
+
+
+
+
+
+
+
